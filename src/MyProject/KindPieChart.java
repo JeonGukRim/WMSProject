f453abe05d9af5a4cbf0338dbc72ced27a22b98c
tj-bu-11 <tj-bu-11@DESKTOP-7GB30DL>
2023-01-27 18:18:36 +0900

    최종 백업

--- src/MyProject/KindPieChart.java
+++ src/MyProject/KindPieChart.java
@@ -6,12 +6,18 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Vector;
+
 import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class KindPieChart extends JLabel {
 	public Connection conn;
 	public Statement stmt = null;
 	private ResultSet rs = null;
+	private Vector data = null;
+	private Vector result;
 
 	public KindPieChart() {
 
@@ -19,8 +25,28 @@ public class KindPieChart extends JLabel {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		
+
+		g.fillArc(0, 0, 100, 100, 0, 30);
+	}
+
+	// SELECT COUNT(DISTINCT sku_kind) from listdb group by sku_kind; 종류
+//	select sku_kind, SUM(sku_finalnum) as total from listdb group by sku_kind;
+	public Vector allData() {
+		data.clear();
+		try {
+			rs = stmt.executeQuery("select sku_kind, SUM(sku_finalnum) as total from listdb group by sku_kind");
+			while (rs.next()) {
+				Vector in = new Vector<String>(); //
+				String kind = rs.getString("sku_kind");
+				String total = rs.getString("total");
+				in.add(kind);
+				in.add(total);
+				data.add(in);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return data;
 	}
 
 	public void dbclass() {
