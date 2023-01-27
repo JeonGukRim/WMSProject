e7a7bd6b51c6692c8f2c948ab84084c1fb06a797
tj-bu-11 <tj-bu-11@DESKTOP-7GB30DL>
2023-01-27 18:20:43 +0900

    최종

--- iohistory.sql
+++ iohistory.sql
@@ -2,12 +2,16 @@
 -- Query: SELECT * FROM myproject.iohistory
 LIMIT 0, 200
 
--- Date: 2023-01-27 12:19
+-- Date: 2023-01-27 18:19
 */
 INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k125','바디로션','화장품','IO20230127120455','입고','2023년01월27일12시06분',30,25,'masterid','over','A-4-2');
 INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k132','선크림바','화장품','IO20230127120514','입고','2023년01월27일12시07분',10,10,'masterid','over','A-4-2');
 INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k126','비비크림','화장품','IO20230127120524','입고','2023년01월27일12시07분',60,50,'masterid','over','A-4-4');
 INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k126','비비크림','화장품','IO20230127120828','출고','2023년01월27일12시08분',20,15,'masterid','over','A-4-4');
 INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k131','립스틱','화장품','IO20230127120916','입고',NULL,6,NULL,NULL,'yet','A-4-2');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k128','휴지','생활용품','IO20230127143118','입고',NULL,6,NULL,NULL,'yet','A-4-7');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k125','바디로션','화장품','IO20230127143428','입고','2023년01월27일14시39분',20,15,'masterid','over','A-4-5');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k125','바디로션','화장품','IO20230127143954','입고','2023년01월27일14시40분',20,20,'masterid','over','A-4-2');
 INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k125','바디로션','화장품','OP20230127120816','출고','2023년01월27일12시08분',20,5,'masterid','over','A-4-2');
 INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k126','비비크림','화장품','OP20230127120907','출고','2023년01월27일12시17분',5,4,'kk11','over','A-4-4');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`,`sku_location`) VALUES ('k126','비비크림','화장품','OP20230127163057','출고',NULL,12,NULL,NULL,'yet','A-4-4');
