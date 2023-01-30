57c4f361d70a90a97c9633a8f7e103bb7bbbfdcf
tj-bu-11 <tj-bu-11@DESKTOP-7GB30DL>
2023-01-30 10:20:07 +0900

    차트 수정중

--- src/MyProject/KindPieChart.java
+++ src/MyProject/KindPieChart.java
@@ -1,5 +1,6 @@
 package MyProject;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,45 +9,75 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Vector;
 
-import javax.swing.JLabel;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class KindPieChart extends JLabel {
+public class KindPieChart extends JFrame {
 	public Connection conn;
 	public Statement stmt = null;
 	private ResultSet rs = null;
-	private Vector data = null;
+	private Vector data1 = new Vector();
+	private Vector<Integer> data2 = new Vector();
 	private Vector result;
+	private MyPanel panel = new MyPanel();
 
 	public KindPieChart() {
-
+		setContentPane(panel);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500, 2020);
+		setVisible(true);
 	}
 
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-
-		g.fillArc(0, 0, 100, 100, 0, 30);
+	class MyPanel extends JPanel {
+		private int sum = 0;
+		private Color[] color = {Color.RED,
+				Color.BLUE,Color.YELLOW,Color.GRAY,Color.CYAN,Color.ORANGE,Color.GREEN};
+		private int start = 0;
+		private int over =0;
+		private	Graphics g = null;
+		public MyPanel() {  
+			dbclass();
+			allData();
+			for(int i =0 ; i < data1.size();i++) {
+					sum += data2.get(i);
+			}
+			
+			for(int i = 0;i<data1.size();i++) {
+				over =((data2.get(i)/sum)*360);
+				paintComponent(g,color[i],start,over);
+				start += over;
+			}
+			
+		}
+		
+		public void paintComponent(Graphics g,Color color,int start,int over) {
+			super.paintComponent(g);
+			g.setColor(color);
+			g.fillArc(0,0,100,100,start,over);
+		}
 	}
 
 	// SELECT COUNT(DISTINCT sku_kind) from listdb group by sku_kind; 종류
 //	select sku_kind, SUM(sku_finalnum) as total from listdb group by sku_kind;
-	public Vector allData() {
-		data.clear();
+	public void allData() {
+		data1.clear();
+		data2.clear();
 		try {
 			rs = stmt.executeQuery("select sku_kind, SUM(sku_finalnum) as total from listdb group by sku_kind");
 			while (rs.next()) {
-				Vector in = new Vector<String>(); //
+				Vector in1 = new Vector<String>(); //
+				Vector in2 = new Vector<Integer>(); //
 				String kind = rs.getString("sku_kind");
-				String total = rs.getString("total");
-				in.add(kind);
-				in.add(total);
-				data.add(in);
+				int total = rs.getInt("total");
+				in1.add(kind);
+				in2.add(total);
+				data1.add(in1);
+				data2.addAll(in2);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return data;
+//		return data;
 	}
 
 	public void dbclass() {
